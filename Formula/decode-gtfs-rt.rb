dd39519c0c6a277418c30eaecf4c21fb279f35a7
Cole McCarren <45370979+colemccarren@users.noreply.github.com>
2024-11-06 01:49:28 -0500

    Update decode-gtfs-rt.rb

--- Formula/decode-gtfs-rt.rb
+++ Formula/decode-gtfs-rt.rb
@@ -1,7 +1,7 @@
 class DecodeGtfsRt < Formula
   desc "Decode GTFS-RT feed into human-readable JSON format"
   homepage "https://github.com/colemccarren/homebrew-decode-gtfs-rt"
-  url "https://github.com/colemccarren/homebrew-decode-gtfs-rt/raw/main/decode-gtfs-rt.tar.gz"
+  url "https://github.com/colemccarren/homebrew-decode-gtfs-rt/releases/download/0.2/decode-gtfs-rt.tar.gz"
   sha256 "67f5828fa544dd463fbb5da2333774a638f3a9eebf582b6e0d7e391a5de7738f"
 
   def install

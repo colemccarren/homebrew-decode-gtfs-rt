bd077ea0489cb95697003e976fe0ca87cdb36d6c
Cole McCarren <45370979+colemccarren@users.noreply.github.com>
2024-11-06 01:47:36 -0500

    Tarball rehash

--- Formula/decode-gtfs-rt.rb
+++ Formula/decode-gtfs-rt.rb
@@ -2,7 +2,7 @@ class DecodeGtfsRt < Formula
   desc "Decode GTFS-RT feed into human-readable JSON format"
   homepage "https://github.com/colemccarren/homebrew-decode-gtfs-rt"
   url "https://github.com/colemccarren/homebrew-decode-gtfs-rt/raw/main/decode-gtfs-rt.tar.gz"
-  sha256 "a3c344b836d98caa9f43e450788c14233d2092c5ec9d61b84f3e9e5c7c10ebda"
+  sha256 "67f5828fa544dd463fbb5da2333774a638f3a9eebf582b6e0d7e391a5de7738f"
 
   def install
     bin.install "decode_gtfs_rt.sh" => "decode"
